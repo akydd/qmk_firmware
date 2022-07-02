e23f7063243117fe6b056fa525bffa77a7c5cd1b
Alan Kydd <akydd@ualberta.net>
2022-07-01 22:33:54 -0600

    Updated mouse movement settings

--- keyboards/planck/keymaps/akydd/config.h
+++ keyboards/planck/keymaps/akydd/config.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
+#    define STARTUP_SONG SONG(PLANCK_SOUND)
+// #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
+#    define DEFAULT_LAYER_SONGS \
+        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
 #endif
 
 /*
@@ -44,3 +42,10 @@
 
 #define RGBLIGHT_LAYERS
 #define HSV_TRON 128, 241, 248
+
+// mouse settings
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
