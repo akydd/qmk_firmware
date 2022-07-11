21109d3666c5a4c4508780a26419bdc6c7c8c1c5
Alan Kydd <akydd@ualberta.net>
2022-07-11 12:45:26 -0600

    Added missing number eight

--- keyboards/planck/keymaps/akydd/keymap.c
+++ keyboards/planck/keymaps/akydd/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [NUM] = LAYOUT_planck_grid(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,            KC_F5,      _______,    _______,    KC_F8,      KC_F9,     KC_F10,   KC_F11,   KC_F12,
-    KC_1,     KC_2,     KC_3,     KC_4,             KC_F6,      _______,    _______,    KC_F7,      KC_7,      KC_9,     KC_9,     KC_0,
+    KC_1,     KC_2,     KC_3,     KC_4,             KC_F6,      _______,    _______,    KC_F7,      KC_7,      KC_8,     KC_9,     KC_0,
     _______,  _______,  _______,  KC_5,             _______,    _______,    _______,    _______,    KC_6,      _______,  _______,  _______,
     _______,  _______,  _______,  TO(COLEMAK_DH),   _______,    _______,    _______,    _______,    OSL(SYM),  _______,  _______,  _______
 ),
